986d1eda1535284f590c64d4c65ac2babf59c79f
Your Name <you@example.com>
2022-09-15 13:19:56 +0200

    getkeresek

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-import express, { request } from 'express';
-import { diakok } from 'nodealapok/adatok.js';
 
-const app = express();
+import express from 'express'
+import mysql from 'mysql'
+import {config} from './dbconfig.js';
+
+const app = express()
 app.use(express.json())
+const db = mysql.createConnection(config)
 
 app.get('/', (request, response) => {
-    // response.send('Saját szerverünk küldi ezt az üzenetet')
-    response.send(diakok)
+    const {author} = request.params
+    
+    db.query('SELECT author from books group by author order by author', (err, result) => {
+        if (err)
+            console.log(err)
+        else
+            response.send(result)
+    })
+})
+app.get('/szerzo/:author', (request, response) => {
+    const {author} = request.params
+    
+    db.query('SELECT author from books group by author=? order by title',[author], (err, result) => {
+        if (err)
+            console.log(err)
+        else
+            response.send(result)
+    })
+})
+app.get('/kateg/:category/:year', (request, response)=>{
+    const {category,year} = request.params
+    
+    db.query('SELECT author,title,year from books where category=? and year>=?',[category,year], (err, result) => {
+        if (err)
+            console.log(err)
+        else
+            response.send(result)
+    })
+})
+
+app.get('/azon/:id', (request, response)=>{
+    const {id} = request.params
+    
+    db.query('SELECT author,title,year,category from books where id=? order by title',[id], (err, result) => {
+        if (err)
+            console.log(err)
+        else
+            response.send(result)
+    })
 })
 
 
-app.get('/:id', (request, response) => {
-    const { id } = request.params //a u url-ben érkező paraméterek elérése
-    const filteredArr = diakok.filter(obj => obj.id == id)
-    response.send(filteredArr)
-})
-
-
-app.post('/', (request, response) => {
-    const { id, nev, osz } = request.body
-    diakok.push({ id: id, nev: nev, osz: osz })
-    response.send(diakok)
-})
-app.get('*', (request, response) => {
-    response.status(404).send('<h1>Az oldal nem létezik....</h1>')
-})
-
-app.listen(5000, () => console.log('Fut a szerver az 5000-res porton!!'))
+app.listen(5000, () => console.log('server listening on port 5000...'))
